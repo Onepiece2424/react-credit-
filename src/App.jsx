bdf907d5c7519e170cbfb114a27282a39fbf238c
rioto.nagano <kingdom950927@gmail.com>
2022-11-03 08:23:44 +0900

    redux-formを用いた入力フォームの作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 
 // components
-import Credit from "./components/Credit"
+import Credit from "./components/modules/Credit"
+import Forming from './components/Forming';
 
 function App() {
   return (
     <>
       <Credit />
+      <br></br>
+      <br></br>
+      <Forming />
     </>
   );
 }
