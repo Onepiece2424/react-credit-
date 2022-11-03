5c101ca939b521f627523dc18506a37b19a6e37c
rioto.nagano <kingdom950927@gmail.com>
2022-11-03 11:17:36 +0900

    redux-formにバリデーションをかける

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,33 +1,57 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Form,FormGroup,Col,FormLabel,Button,ButtonToolbar } from 'react-bootstrap';
+// import { Form,FormGroup,Col,FormLabel,Button,ButtonToolbar } from 'react-bootstrap';
+
+const required = value => value ? undefined : 'Required'
+const maxLength = max => value =>
+  value && value.length > max ? `Must be ${max} characters or less` : undefined
+const maxLength15 = maxLength(15)
+const number = value => value && isNaN(Number(value)) ? 'Must be a number' : undefined
+const minValue = min => value =>
+  value && value < min ? `Must be at least ${min}` : undefined
+const minValue18 = minValue(18)
+const email = value =>
+  value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+  'Invalid email address' : undefined
+const tooOld = value =>
+  value && value > 65 ? 'You might be too old for this' : undefined
+const aol = value =>
+  value && /.+@aol\.com/.test(value) ?
+  'Really? You still use AOL for your email?' : undefined
+
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <label>{label}</label>
+    <div>
+      <input {...input} placeholder={label} type={type}/>
+      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+)
 
 const MyForm = props => {
   const { handleSubmit, pristine, reset, submitting } = props;
   return (
-    <Form horizontal onSubmit={handleSubmit}>
-      <FormGroup controlId={'name'}>
-        <Col componentClass={FormLabel} sm={2}>お名前</Col>
-        <Col sm={5}>
-          <Field
-            id={'name'}
-            name="name"
-            component="input"
-            type="text"
-            placeholder="Name"
-            className={'form-control'}
-          />
-        </Col>
-      </FormGroup>
-      <FormGroup>
-        <Col smOffset={2} sm={5}>
-          <ButtonToolbar>
-            <Button bsStyle={'primary'} type="submit" disabled={pristine || submitting}>登録</Button>
-            <Button type="button" disabled={pristine || submitting} onClick={reset}>クリア</Button>
-          </ButtonToolbar>
-        </Col>
-      </FormGroup>
-    </Form>
+    <form onSubmit={handleSubmit}>
+      <Field name="username" type="text"
+        component={renderField} label="Username"
+        validate={[ required, maxLength15 ]}
+      />
+      <Field name="email" type="email"
+        component={renderField} label="Email"
+        validate={email}
+        warn={aol}
+      />
+      <Field name="age" type="number"
+        component={renderField} label="Age"
+        validate={[ required, number, minValue18 ]}
+        warn={tooOld}
+      />
+      <div>
+        <button type="submit" disabled={submitting}>Submit</button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>Clear Values</button>
+      </div>
+    </form>
   );
 };
 
