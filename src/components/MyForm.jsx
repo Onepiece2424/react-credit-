14c393afb6ecf54e8cba0efbc8206b400c10d48b
rioto.nagano <kingdom950927@gmail.com>
2022-11-03 12:01:35 +0900

    validationエラーメッセージをcomponentとして切り出す

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 // import { Form,FormGroup,Col,FormLabel,Button,ButtonToolbar } from 'react-bootstrap';
 
+import renderField from './renderField';
+
 const required = value => value ? undefined : 'Required'
 const maxLength = max => value =>
   value && value.length > max ? `Must be ${max} characters or less` : undefined
@@ -18,16 +20,6 @@ const tooOld = value =>
 const aol = value =>
   value && /.+@aol\.com/.test(value) ?
   'Really? You still use AOL for your email?' : undefined
-
-const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <input {...input} placeholder={label} type={type}/>
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
-    </div>
-  </div>
-)
 
 const MyForm = props => {
   const { handleSubmit, pristine, reset, submitting } = props;
