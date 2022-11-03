c31337b00a20614089573c714bb91ceee519780e
rioto.nagano <kingdom950927@gmail.com>
2022-11-03 20:11:23 +0900

    非同期処理でテキスト表示

--- src/components/AsyncAlert.jsx
+++ src/components/AsyncAlert.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 
 const AsyncAlert = (() => {
 
-  function syncAlert() {
-    setTimeout(() => {alert("まだだ。。。")},0)
-    console.log("今だ！いけー")
+  // 非同期処理の作成
+  async function showJsonText() {
+    const response = await fetch ('http://127.0.0.1:5500/test.json');
+    const json = await response.json();
+    const resultBox = document.querySelector('.result');
+    resultBox.textContent = json.text;
   }
-  syncAlert();
+  showJsonText();
 
   return (
     <>
-      <div>今日も一日お疲れ様。</div>
+      <div className='result'>今日も一日お疲れ様。</div>
     </>
   )
 })
