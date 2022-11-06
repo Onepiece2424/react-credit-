767d74a8535b9c2241a9d401a685ad8b17c521ef
rioto.nagano <kingdom950927@gmail.com>
2022-11-06 15:15:19 +0900

    initialStateの記述位置の変更

--- src/stores/index.js
+++ src/stores/index.js
@@ -4,22 +4,29 @@ import { reducer as reduxFormReducer } from 'redux-form';
 // initialStateの作成
 const initialState = {
   count: 100,
-//   user: {
-//     name: "田中",
-//     job: "学生",
-//     age: 21,
-//     family: ["ひろし", "かずえ", "きょうこ"]
-//   },
-//   animal: {
-//     name: "ライオン",
-//     size: "2m",
-//     color: "yellow"
-//   },
-//   fruits: [
-//     { id: 1, fruit_name: "りんご", fruit_color: "red", quantity: 3 },
-//     { id: 2, fruit_name: "みかん", fruit_color: "orange", quantity: 1 },
-//     { id: 3, fruit_name: "パイナップル", fruit_color: "yellow", quantity: 5 },
-//   ]
+  user: {
+    name: "さとる",
+    job: "学生",
+    age: 21,
+    familys: [
+      { id: 1, attribute: "父", name: "のぼる", favorite_food: "ラーメン", favorite_number: 7 },
+      { id: 2, attribute: "母", name: "かずえ", favorite_food: "カレー", favorite_number: 11 },
+      { id: 3, attribute: "妹", name: "かりん", favorite_food: "すし", favorite_number: 9 },
+    ],
+    greeting: function() {
+      console.log("おはよ")
+    }
+  },
+  animal: {
+    name: "ライオン",
+    size: "2m",
+    color: "yellow"
+  },
+  fruits: [
+    { id: 1, fruit_name: "りんご", fruit_color: "red", quantity: 3 },
+    { id: 2, fruit_name: "みかん", fruit_color: "orange", quantity: 1 },
+    { id: 3, fruit_name: "パイナップル", fruit_color: "yellow", quantity: 5 },
+  ]
 };
 
 
@@ -43,47 +50,15 @@ const countReducer = (state = initialState, action) => {
   }
 };
 
-const userReducer = (
-  state = {
-    user: {
-      name: "さとる",
-      job: "学生",
-      age: 21,
-      familys: [
-        { id: 1, attribute: "父", name: "のぼる", favorite_food: "ラーメン", favorite_number: 7 },
-        { id: 2, attribute: "母", name: "かずえ", favorite_food: "カレー", favorite_number: 11 },
-        { id: 3, attribute: "妹", name: "かりん", favorite_food: "すし", favorite_number: 9 },
-      ],
-      greeting: function() {
-        console.log("おはよ")
-      }
-    }
-  }
-) => {
+const userReducer = (state = initialState) => {
   return state
 };
 
-const animalReducer = (
-  state = {
-    animal: {
-      name: "ライオン",
-      size: "2m",
-      color: "yellow"
-    }
-  }
-) => {
+const animalReducer = (state = initialState) => {
   return state
 };
 
-const fruitReducer = (
-  state = {
-    fruits: [
-      { id: 1, fruit_name: "りんご", fruit_color: "red", quantity: 3 },
-      { id: 2, fruit_name: "みかん", fruit_color: "orange", quantity: 1 },
-      { id: 3, fruit_name: "パイナップル", fruit_color: "yellow", quantity: 5 },
-    ]
-  }
-) => {
+const fruitReducer = (state = initialState) => {
   return state;
 }
 
