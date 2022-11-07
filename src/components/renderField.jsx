14fcb2e92443d9f37da6b09b4e8d68cc2bb6033c
rioto.nagano <kingdom950927@gmail.com>
2022-11-08 06:19:18 +0900

    renderFieldのpropswp分割代入

--- src/components/renderField.jsx
+++ src/components/renderField.jsx
@@ -1,13 +1,18 @@
 import React from "react"
 
-const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
-  <div>
-    <label>{label}</label>
-    <div className='alert-message'>
-      <input {...input} placeholder={label} type={type}/>
-      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+const renderField = ((props) => {
+
+  const { input, label, type, meta: { touched, error, warning } } = props
+
+  return (
+    <div>
+      <label>{label}</label>
+      <div className='alert-message'>
+        <input {...input} placeholder={label} type={type}/>
+        {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+      </div>
     </div>
-  </div>
-);
+  )
+});
 
 export default renderField;
