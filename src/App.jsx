af20f03c0015f097394d74d6eb2e971a4b2b33f2
rioto.nagano <kingdom950927@gmail.com>
2022-11-27 21:44:45 +0900

    fetchメソッドを使用し外部データの取得

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
+// css
 import './App.css';
 
 // components
 // import Credit from "./components/Credit"
 // import Forming from './components/Forming';
 // import AsyncAlert from './components/AsyncAlert';
-
 // import ReduxLearn from './components/ReduxLearn'
 // import SampleForm from './components/SampleForm'
-
 // import ValidationForm from './components/ValidationForm';
 // import {Container,Row,Col} from 'react-bootstrap';
 // import HookForm from './components/HookForm';
 import ReactCredit from './components/ReactCredit';
+import ApiFetch from './components/ApiFetch';
 
 function App() {
 
   return (
     <div className="App">
       <ReactCredit />
+      <ApiFetch />
     </div>
   );
 }
