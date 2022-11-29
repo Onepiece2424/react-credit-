3c1f8dd03e0af34b013211cbb319b272f05c40be
rioto.nagano <kingdom950927@gmail.com>
2022-11-29 22:16:19 +0900

    不要な部分の削除

--- src/App.jsx
+++ src/App.jsx
@@ -10,15 +10,14 @@ import './App.css';
 // import ValidationForm from './components/ValidationForm';
 // import {Container,Row,Col} from 'react-bootstrap';
 // import HookForm from './components/HookForm';
+// import ApiFetch from './components/ApiFetch';
 import ReactCredit from './components/ReactCredit';
-import ApiFetch from './components/ApiFetch';
 
 function App() {
 
   return (
     <div className="App">
       <ReactCredit />
-      <ApiFetch />
     </div>
   );
 }
