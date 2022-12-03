ba71b2eda0c851c29df84fa8ff73500d6bf6efaf
rioto.nagano <kingdom950927@gmail.com>
2022-12-03 10:33:05 +0900

    recapture v3をコンポーネント全体で使用できるようにする

--- src/index.js
+++ src/index.js
@@ -11,9 +11,14 @@ import store from "./stores/index";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
+// recapture v3
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <GoogleReCaptchaProvider reCaptchaKey={process.env.REACT_APP_RECAPTCHA_SITE_KEY_V3}>
+      <App />
+    </GoogleReCaptchaProvider>
   </Provider>,
   document.getElementById("root")
 );
